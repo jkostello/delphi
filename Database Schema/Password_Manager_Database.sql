de0f7113b145aeda9620b0f714503887ac4aab91
Kolton Kehoe <160361420+KoltonKehoe@users.noreply.github.com>
2024-03-28 21:57:49 -0500

    Created MySQL Database Schema for Password Manager
    The is the schema for the database to hold user information and passwords. It creates two tables, one for user info. and the other for all of the passwords. Each password has its own id and has an id for the user it is connected to. For the users, they also have an id and other info. like their master pass, pass hash, and token.

---
+++ Database Schema/Password_Manager_Database.sql
@@ -0,0 +1,21 @@
+-- Creates the database
+CREATE database if not exists PasswordDatabase;
+
+-- Uses the new database for creating the tables to hold data
+Use PasswordDatabase;
+
+-- create table for users
+create table if not exists users (
+	user_id int unsigned auto_increment primary key,
+	username varchar(255),
+    user_master_password varchar(255),
+    password_hash varchar(255),
+    token varchar(255)
+    );
+    
+-- create table for passwords
+create table if not exists passwords (
+	passwords_id int unsigned auto_increment primary key,
+    user_id int unsigned not null,
+    user_password varchar(255) not null
+    );
