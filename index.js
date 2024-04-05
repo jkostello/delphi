459976543f56dfb77d87b8d2a94986850126c3e3
nmcph04 <nmcpherson2004@gmail.com>
2024-04-05 16:33:30 -0500

    Added HTTP request functionality

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-require('dotenv').config()
-
 const express = require('express')
+const path = require('path')
+const httpProxy = require('http-proxy')
+
+const SERVER_URL = "127.0.0.1"
+const PROXY_PORT = 4000
+
 const app = express()
-const jwt = require('jsonwebtoken')
-const mysql = require('mysql')
-const argon2 = require('argon2')
+const proxy = httpProxy.createProxyServer()
 
-/*
-
-const db = mysql.createConnection({
-    'host': process.env.DATABASE_HOST,
-    'user': process.env.DATABASE_USER,
-    'password': process.env.DATABASE_PASSWORD,
-    'database': process.env.DATABASE
-})
-
-db.connect((error) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log("Connected to database!")
+const forwardPostRequests = (req, res, next) => {
+    // Forward POST requests to auth server
+    if (req.method === 'POST') {
+        let forwardTarget = req.headers.referer.split(PORT).slice(-1)[0]
+        let fullForwardTarget = "http://"+SERVER_URL+":"+PROXY_PORT+forwardTarget
+        
+        proxy.web(req, res, { target: fullForwardTarget }, err => {
+            console.log("Error while forwarding POST request:", err)
+            res.status(500).send("Proxy error")
+        })
     }
-})
-*/
+    else {
+        next()
+    }
+}
 
 app.use(express.json())
+app.use(express.static(__dirname + '/public'))
 
 // Add GET responses for the website pages here
-//app.get("/", (req, res) => {
-//
-//})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+})
 
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
 
+app.use(forwardPostRequests)
 
-const port = 3000
-app.listen(port)
-console.log("Now listening on port " + port + ". ")
+const PORT = 3000
+app.listen(PORT)
+console.log("Now listening on port " + PORT + ". ")
